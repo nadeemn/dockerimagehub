a337d2997544a543529c4d9891d6a489dde3daf1
nadeemn <nadeemnazer123@gmail.com>
2024-09-08 18:32:21 +0200

    remove test conflict.

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     response = app.test_client().get("/")
     
     assert response.status_code == 200
-    assert response.data == "hello World"
+    assert response.data == b"hello World"
